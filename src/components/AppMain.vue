2243cf7ae2081bb07b3c552c5bb9ad3d3604fa99
Leograzioli <leograzioli@gmail.com>
2022-11-10 21:33:50 +0100

    aggiunto tag main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,23 +16,25 @@ export default {
 </script>
 
 <template>
-    <div class="container p-5 bg-white">
-        <div class="row rwo-cols-12 ms_bg-secondary text-white p-3">
-            <span v-if="store.loaded">
-                Found {{ store.characters.length }} characters
-            </span>
-            <span v-else>
-                Loading.. please wait
-            </span>
-        </div>
-        <div class="row row-cols-2 row-cols-md-5 p-4">
-            <div v-for="item in store.characters " :key="item.char_id"
-                class="col d-flex justify-content-center g-3 text-white">
-                <CardCharacter :character="item" />
+    <main>
+        <div class="container p-5 bg-white">
+            <div class="row rwo-cols-12 ms_bg-secondary text-white p-3">
+                <span v-if="store.loaded">
+                    Found {{ store.characters.length }} characters
+                </span>
+                <span v-else>
+                    Loading.. please wait
+                </span>
             </div>
-        </div>
+            <div class="row row-cols-2 row-cols-md-5 p-4">
+                <div v-for="item in store.characters " :key="item.char_id"
+                    class="col d-flex justify-content-center g-3 text-white">
+                    <CardCharacter :character="item" />
+                </div>
+            </div>
 
-    </div>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
